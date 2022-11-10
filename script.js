c2c1776346cb347eb92d71faa1f6e68def4f0dcd
Anthony Cedrone <anthonycedrone@Anthonys-Air.fios-router.home>
2022-11-10 09:58:58 -0500

    commented on script.js and found a solution that worked

--- script.js
+++ script.js
@@ -11,16 +11,23 @@ var passArr = [];
 // WHEN I click the button to generate a password
 function generatePassword() {
 
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
   var length = prompt("Choose a length of at least 8 characters and no more than 128 characters")
   if (length >= 8 && length <= 128) {
 
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
     var selectLowercase = confirm("Would you like to include lowercase characters?")
     var selectUppercase = confirm("Would you like to include uppercase characters?")
     var selectNumber = confirm("Would you like to include numbers?")
     var selectSpecial = confirm("Would you like to include special characters?")
 
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// Added each kind of character one by one, but because it can be up to 4 types, it exceeds the chosen length
     for (var i = 0; i < length; i++) {
-
+// If this type of character is selected, then add one of them to the password
       if (selectLowercase) {
         var randomNum = Math.floor(Math.random() * lowercase.length);
         randomArr.push(lowercase.substring(randomNum, randomNum + 1));
@@ -42,26 +49,21 @@ function generatePassword() {
         alert("You must select at least one character type")
       }
     }
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// Pulling only the chosen length amount of characters from previous password array, assuring each kind of charaacter is used
     for (var i = 0; i < length; i++) {
       passArr.push(randomArr[i])
     }
+// Joining the new array into a string
     var withoutCommas = passArr.join('');
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
     return withoutCommas;
   } else {
     alert("You must enter a length of at least 8 characters and no more than 128 characters")
   }
 }
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
 
 // Write password to the #password input
 function writePassword() {
